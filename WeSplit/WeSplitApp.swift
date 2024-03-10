73cb13211b1bc142e2f57644fc4177b8ea62f07f
Igor Shcherba <igor.shcherba@callstack.com>
2024-03-10 13:08:23 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Igor Shcherba on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
